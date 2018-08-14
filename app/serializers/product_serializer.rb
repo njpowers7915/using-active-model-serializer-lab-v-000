1e5296ca86db3955a9286f9568d4696daa52ab7b
Nicholas Powers <njpowers7915@gmail.com>
2018-08-14 15:47:43 +0000

    automatically backed up by learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,4 +1,4 @@
 class ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :inventory, :price
-  has_many :orders, serializer: ProductOrderSerializer
+  has_many :orders
 end
